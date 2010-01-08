8a8d1fea3d4ac7ef0a3bb3a44bb891d9ae3a0995
Charles Duffy <charles@dyfis.net>
2010-01-08 10:05:03 -0600

    add TCX support, full-featured template, other misc work

--- animsvg.py
+++ animsvg.py
@@ -98,16 +98,16 @@ class Converter(object):
                 break
             data = self.get_data()
             buf = self.filter_buffer(buf, **data)
-            print ' --- Completed frame {0} ({1:.2f}%); {2} bytes'.format(frameno, (float(data['stream_position']) / float(data['stream_duration'])) * 100, len(buf))
+            print ' --- Completed frame {0} ({1:.2f}%); {2} bytes'.format(frameno, (float(data['video_stream_position']) / float(data['video_stream_duration'])) * 100, len(buf))
             self.png_src.emit('push_buffer', buf)
             frameno += 1
         loop.quit()
     def get_data(self):
         retval = {}
-        retval['stream_position'] = self.player.query_position(self.time_format, None)[0]
-        retval['stream_duration'] = self.player.query_duration(self.time_format, None)[0]
-        retval['width'] = str(self.app_caps[0]['width'])
-        retval['height'] = str(self.app_caps[0]['height'])
+        retval['video_stream_position'] = self.player.query_position(self.time_format, None)[0]
+        retval['video_stream_duration'] = self.player.query_duration(self.time_format, None)[0]
+        retval['video_width'] = str(self.app_caps[0]['width'])
+        retval['video_height'] = str(self.app_caps[0]['height'])
         return retval
     def filter_buffer(self, buffer_in, **kwargs):
         # TODO: actually write this
@@ -161,7 +161,7 @@ class SVGConverter(Converter):
     def filter_buffer(self, buffer_in, **kwargs):
         # TODO: lock?
         kwargs['input_frame_filename'] = self.tempfile_png_in.name
-        kwargs.update(self.get_data_for_time(kwargs['stream_position']))
+        kwargs.update(self.get_data_for_time(kwargs['video_stream_position']))
 
         self.tempfile_png_in.seek(0)
         self.tempfile_png_in.truncate()
@@ -175,8 +175,8 @@ class SVGConverter(Converter):
 
         subprocess.check_call([
             'rsvg',
-            '-w', kwargs['width'],
-            '-h', kwargs['height'],
+            '-w', kwargs['video_width'],
+            '-h', kwargs['video_height'],
             self.tempfile_svg.name,
             self.tempfile_png_out.name,
         ])
@@ -190,8 +190,6 @@ class SVGConverter(Converter):
 
 # YYYY-MM-DDTHH:MM:SS
 TIME_FMT_STR='%Y-%m-%dT%H:%M:%S'
-GPX_NS='http://www.topografix.com/GPX/1/1'
-GPX='{%s}' % GPX_NS
 
 def timedelta_to_seconds(td):
     return (
@@ -200,24 +198,148 @@ def timedelta_to_seconds(td):
         td.microseconds * 0.000001
     )
 
-class GPXConverter(SVGConverter):
-    def __init__(self, video_in, video_out, svg_template_text, gpx_file_name, video_start_time):
+class XMLConverter(SVGConverter):
+    """A base class providing common tools for XML-based trackpoint storage formats"""
+    def __init__(self, video_in, video_out, svg_template_text, xml_file_name, video_start_time):
         SVGConverter.__init__(self, video_in, video_out, svg_template_text)
         if isinstance(video_start_time, basestring):
             video_start_time = datetime.strptime(video_start_time, TIME_FMT_STR)
         self.start_time = video_start_time
-        self.gpx_data = lxml.etree.parse(open(gpx_file_name))
-        # set up state
-        self.gpx_data_iter = self.get_trackpoint_iterator()
-        self.prev_point = self.gpx_data_iter.next()
-        self.next_point = self.gpx_data_iter.next()
+        self.xml_data = lxml.etree.parse(open(xml_file_name))
+        self.xml_data_iter = self.get_trackpoint_iterator()
+        self.prev_point = self.xml_data_iter.next()
+        self.next_point = self.xml_data_iter.next()
+        self.data_start_time = self.prev_point['time']
         self.update_span_stats()
+        self.init_total_stats()
+        self.finished = False # if data stream ended
+    def get_trackpoint_iterator(self):
+        raise NotImplementedError
+    def init_total_stats(self):
         self.prior_dist = 0.0 # distance previously traveled
         self.climb = 0.0
         self.descent = 0.0
-        self.finished = False # if data stream ended
+    def update_curr_dist(self):
+        """If this format does not contain distance from a more accurate source, calculate it here"""
+        ## TODO: factor elevation delta into distance
+        self.curr_dist = geolocator.gislib.getDistance(
+            (self.prev_point['lon'], self.prev_point['lat']),
+            (self.next_point['lon'], self.next_point['lat']),
+        ) * (geolocator.gislib.kmsPerNauticalMile * 1000)
+    def update_span_stats(self):
+        """ Update statistics related to the current and next points; does not alter totals.  """
+        self.update_curr_dist()
+        self.elevation_delta = self.next_point['ele'] - self.prev_point['ele']
+        self.curr_span = self.next_point['time'] - self.prev_point['time']
+        self.curr_span_seconds = timedelta_to_seconds(self.curr_span)
+        self.curr_speed = self.curr_dist / self.curr_span_seconds
+        # determine prev, next point times as seconds from start of video
+        self.prev_point_seconds = timedelta_to_seconds(self.prev_point['time'] - self.start_time)
+        self.next_point_seconds = timedelta_to_seconds(self.next_point['time'] - self.start_time)
+        # determine grade
+        if self.curr_dist == 0:
+            self.curr_grade = 0
+        else:
+            self.curr_grade = self.elevation_delta / ((abs((self.curr_dist ** 2) - (self.elevation_delta ** 2)) ** 0.5) * (-1 if self.elevation_delta < 0 else 1))
+    def update_totals(self):
+        if self.elevation_delta > 0:
+            self.climb += self.elevation_delta
+        else:
+            self.descent -= self.elevation_delta
+        self.prior_dist += self.curr_dist
+    def get_data_for_time(self, video_time_ns):
+        """
+        Input:  time in nanoseconds since start of video playback
+        Output: elevation, distance traveled, current speed
+        """
+        video_time_delta = timedelta(microseconds=video_time_ns / 1000)
+        video_time = self.start_time + video_time_delta
+
+        retval = {
+            'cadence': None,
+            'data_available': False,
+            'data_start_time': self.data_start_time,
+            'dist': None,
+            'elevation': None,
+            'grade': None,
+            'heartrate': None,
+            'speed_v': None,
+            'time': video_time,
+            'watts': None,
+        }
+
+        if self.finished or video_time < self.prev_point['time']:
+            return retval
+        while video_time > self.next_point['time'] or self.prev_point['time'] == self.next_point['time']:
+            # update totals before swapping current and next points
+            self.update_totals()
+
+            # ...run the swap...
+            self.prev_point = self.next_point
+            try:
+                self.next_point = self.xml_data_iter.next()
+            except StopIteration:
+                self.finished = True
+                return retval
+            if self.prev_point['time'] == self.next_point['time']:
+                continue
+
+            # ...and update local data
+            self.update_span_stats()
+        # determine frame-local data
+        percent_span_completion = (
+            (timedelta_to_seconds(video_time_delta) - self.prev_point_seconds)
+            / self.curr_span_seconds
+        )
+        retval.update({
+            'curr_span_seconds': self.curr_span_seconds,
+            'curr_span': self.curr_span,
+            'data_available': True,
+            'dist': self.prior_dist + (self.curr_dist * percent_span_completion),
+            'elevation_delta': self.next_point['ele'] - self.prev_point['ele'],
+            'elevation': self.prev_point['ele'] + (self.elevation_delta * percent_span_completion),
+            'grade': self.curr_grade,
+            'speed': self.curr_speed,
+        })
+        return retval
+
+TCX_NS='http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'
+TCX='{%s}' % TCX_NS
+
+class TCXConverter(XMLConverter):
     def get_trackpoint_iterator(self):
-        for trkseg in lxml.etree.ETXPath('//{GPX}trkseg'.format(GPX=GPX))(self.gpx_data):
+        for trkseg in lxml.etree.ETXPath('//{TCX}Track'.format(TCX=TCX))(self.xml_data):
+            for trkpt in trkseg.getchildren():
+                time_str = trkpt.find('{TCX}Time'.format(TCX=TCX)).text.rstrip('Z')
+                time_obj = datetime.strptime(time_str, TIME_FMT_STR)
+                cadence_el = trkpt.find('{TCX}Cadence'.format(TCX=TCX))
+                data = {
+                    'ele': float(trkpt.find('{TCX}AltitudeMeters'.format(TCX=TCX)).text),
+                    'distance': float(trkpt.find('{TCX}DistanceMeters'.format(TCX=TCX)).text),
+                    'cadence': float(cadence_el.text) if cadence_el is not None else None,
+                    'time': time_obj,
+                }
+                yield data
+    def update_curr_dist(self):
+        self.curr_dist = self.next_point['distance'] - self.prev_point['distance']
+    def get_data_for_time(self, video_time_ns):
+        """
+        Input:  time in nanoseconds since start of video playback
+        Output: elevation, distance traveled, current speed
+        """
+        retval = XMLConverter.get_data_for_time(self, video_time_ns)
+        retval.update({
+            'cadence': self.prev_point['cadence'],
+            'input_format': 'tcx',
+        })
+        return retval
+
+GPX_NS='http://www.topografix.com/GPX/1/1'
+GPX='{%s}' % GPX_NS
+
+class GPXConverter(XMLConverter):
+    def get_trackpoint_iterator(self):
+        for trkseg in lxml.etree.ETXPath('//{GPX}trkseg'.format(GPX=GPX))(self.xml_data):
             for trkpt in trkseg.getchildren():
                 time_str = trkpt.find('{GPX}time'.format(GPX=GPX)).text.rstrip('Z')
                 time_obj = datetime.strptime(time_str, TIME_FMT_STR)
@@ -228,61 +350,11 @@ class GPXConverter(SVGConverter):
                     'time': time_obj
                 }
                 yield data
-    def update_span_stats(self):
-        """ Update statistics related to the current and next points; does not alter totals.  """
-        self.elevation_delta = self.next_point['ele'] - self.prev_point['ele']
-        self.curr_dist = geolocator.gislib.getDistance(
-            (self.prev_point['lon'], self.prev_point['lat']),
-            (self.next_point['lon'], self.next_point['lat']),
-        ) * (geolocator.gislib.kmsPerNauticalMile * 1000)
-        self.curr_span = self.next_point['time'] - self.prev_point['time']
-        self.curr_span_seconds = timedelta_to_seconds(self.curr_span)
-        self.curr_speed = self.curr_dist / self.curr_span_seconds
-        # determine prev, next point times as seconds from start of video
-        self.prev_point_seconds = timedelta_to_seconds(self.prev_point['time'] - self.start_time)
-        self.next_point_seconds = timedelta_to_seconds(self.next_point['time'] - self.start_time)
-    def get_data_for_time(self, video_time_ns):
-        """
-        Input:  time in nanoseconds since start of video playback
-        Output: elevation, distance traveled, current speed
-        """
-        video_time_delta = timedelta(microseconds=video_time_ns / 1000)
-        video_time = self.start_time + video_time_delta
-        if self.finished or video_time < self.prev_point['time']:
-            return { 'data_available': False, }
-        while video_time > self.next_point['time']:
-            # determine span-global data
-            if self.elevation_delta > 0:
-                self.climb += self.elevation_delta
-            else:
-                self.descent -= self.elevation_delta
-            self.prev_point = self.next_point
-            try:
-                self.next_point = self.gpx_data_iter.next()
-            except StopIteration:
-                self.finished = True
-                return { 'data_available': False, }
-            self.prior_dist += self.curr_dist
-            self.update_span_stats()
-        # determine frame-local data
-        percent_span_completion = (
-            (timedelta_to_seconds(video_time_delta) - self.prev_point_seconds)
-            / self.curr_span_seconds
-        )
-        return {
-            'data_available': True,
-            'time': video_time,
-            'speed': self.curr_speed,
-            'elevation': self.prev_point['ele'] +
-                (self.elevation_delta * percent_span_completion),
-            'dist': self.prior_dist +
-                (self.curr_dist * percent_span_completion),
-        }
 
 def main():
     gobject.threads_init()
     loop = gobject.MainLoop()
-    c = GPXConverter(sys.argv[1], sys.argv[2], open(sys.argv[3]).read(), sys.argv[4], sys.argv[5])
+    c = TCXConverter(sys.argv[1], sys.argv[2], open(sys.argv[3]).read(), sys.argv[4], sys.argv[5])
     thread.start_new_thread(c.run, (loop,))
     loop.run()
 
